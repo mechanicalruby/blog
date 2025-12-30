e74c74a3454f50fe3dad80f84ade2f4f6791164e
mechanicalruby <robbierocket05@gmail.com>
2025-12-29 20:11:40 -0800

    build script: return zero

--- build.py
+++ build.py
@@ -111,3 +111,5 @@ for filename in glob.glob(input_path + blog_path + '*.md'):
 
     if(compile_blog_page_markdown(Path(filename), output_path + blog_path + page_name + "/index.html")):
         print(Path(filename).name + " -> " + output_path + blog_path + page_name + "/index.html")
+
+sys.exit(0)
