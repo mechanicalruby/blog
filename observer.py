f6e3012e7ceade7be7589d8cf1d4ce30e59e2de6
mechanicalruby <robbierocket05@gmail.com>
2026-05-16 14:07:32 -0700

    build script: watch more files

--- observer.py
+++ observer.py
@@ -6,6 +6,9 @@ builder.generate_site()
 server = Server()
 
 server.watch(builder.input_path + "**/*", builder.generate_site)
+server.watch("styles.css", builder.generate_site)
+server.watch("t_footer.html", builder.generate_site)
+server.watch("t_header.html", builder.generate_site)
 server.watch(builder.output_path + "*.html")
 
 server.serve(port=8000, root=builder.output_path)
